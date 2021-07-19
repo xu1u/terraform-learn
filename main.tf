a803ab7267a852445a7b8124217e755c5128120e
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 20:00:11 +0300

    Added myapp Subnet

--- main.tf
+++ main.tf
@@ -17,3 +17,14 @@ resource "aws_vpc" "myapp-vpc" {
     Name = "${var.env_prefix}-vpc"
   }
 }
+
+# Create Subnet: myapp
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
