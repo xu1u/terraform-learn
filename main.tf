9eaeb220786bc485eac7305f95b0747eca1ec8df
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 19:58:17 +0300

    Added Terraform Variables

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+# Configure Variables: myapp
+variable "vpc_cidr_block"{}
+variable "subnet_cidr_block"{}
+variable "avail_zone"{}
+variable "env_prefix"{}
