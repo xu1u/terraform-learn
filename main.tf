dde66fca8acb5d1f27f9b39dd0296bc7b50a8360
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 19:59:37 +0300

    Added myapp VPC

--- main.tf
+++ main.tf
@@ -8,3 +8,12 @@ variable "vpc_cidr_block"{}
 variable "subnet_cidr_block"{}
 variable "avail_zone"{}
 variable "env_prefix"{}
+
+# Create VPC: myapp
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
