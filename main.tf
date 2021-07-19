8c5baf453a77163bbc031f1f7536d9ce5d09cf88
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 19:56:26 +0300

    Added Terraform AWS Provider

--- main.tf
+++ main.tf
@@ -2,27 +2,3 @@
 provider "aws" {
   region = "eu-central-1"
 }
-
-# Configure Variables: myapp
-variable "vpc_cidr_block"{}
-variable "subnet_cidr_block"{}
-variable "avail_zone"{}
-variable "env_prefix"{}
-
-# Create VPC: myapp
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "${var.env_prefix}-vpc"
-  }
-}
-
-# Create Subnet: myapp
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id     = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
