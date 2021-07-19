f354786f78017eac7fb161f58984a4083f9cecab
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 21:24:34 +0300

    Assign AWS Default Components: Default Route Table

--- main.tf
+++ main.tf
@@ -51,3 +51,16 @@ resource "aws_internet_gateway" "myapp-igw" {
     Name = "${var.env_prefix}-igw"
   }
 }
+
+# Use AWS Default Components: Default Route Table
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
