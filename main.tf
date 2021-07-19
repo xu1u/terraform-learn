d063ef44ff921de818051d2807bbb3631bdccf87
Shaka Ondere <shakaondere@gmail.com>
2021-07-19 20:02:26 +0300

    Added Route Table

--- main.tf
+++ main.tf
@@ -28,3 +28,17 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+# Create Route Table: myapp
+resource "aws_route_table" "myapp_route_table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
