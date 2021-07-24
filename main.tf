e71929f4d66e31a63b246c8291f5bdfae741af88
Shaka Ondere <shakaondere@gmail.com>
2021-07-25 00:58:32 +0300

    Created Subnet Module

--- main.tf
+++ main.tf
@@ -12,51 +12,14 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-# Create Subnet: myapp
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id     = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
-
-/*# Create Route Table: myapp
-resource "aws_route_table" "myapp_route_table" {
+module "myapp-subnet" {
+  source = "modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-rtb"
-  }
-}*/
-
-# Create Internet Gateway: myapp
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
-}
-
-# Use AWS Default Components: Default Route Table
-resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-main-rtb"
-  }
 }
 
 /*# Create Security Group for Firewall Configuration
